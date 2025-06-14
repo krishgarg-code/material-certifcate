9b060bf90c4116ff596ab01c5d9b9e6e0f67b74d
krishgarg-code <krishg0150@gmail.com>
2025-06-14 23:00:07 +0530

    some changes

--- src/components/CertificatePreview.tsx
+++ src/components/CertificatePreview.tsx
@@ -9,13 +9,23 @@ import image4 from './image4.png';
 import roll from './roll.png';
 
 const CertificatePreview = ({ data }) => {
+  // Add function to format decimal values
+  const formatDecimal = (value) => {
+    if (!value) return '';
+    // If the value starts with a decimal point, add a leading zero
+    if (value.startsWith('.')) {
+      return `0${value}`;
+    }
+    return value;
+  };
+
   return (
     <Card className="shadow-none" id="certificate-preview">
       <CardContent className="p-0">
           <div className="border-4 border-black h-full p-4 flex flex-col justify-between m-2 mb-1 ml-2 relative">
             {/* Watermark Roll Image */}
             <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-              <img src={roll} alt="Roll" className="w-85 h-85 object-contain opacity-50" />
+              <img src={roll} alt="Roll" className="w-85 h-85 object-contain opacity-25" />
             </div>
 
             {/* Header */}
@@ -23,22 +33,23 @@ const CertificatePreview = ({ data }) => {
               <div className="w-20 h-20 flex items-center justify-center">
                 <img src={logo} alt="Company Logo" className="max-w-full max-h-full object-contain" />
               </div>
-              <h1 className="text-2xl font-bold text-center  ml-20 -mt-4">Material</h1>
+              <h1 className="text-2xl font-bold text-center  ml-20 -mt-4">Composition </h1>
               <h1 className="text-2xl font-bold  flex-1 ml-5  -mt-4 ">Certificate</h1>
             </div>
 
             {/* Company/Party */}
             <div className="grid  text-xs gap-10 mb-8 ">
               <div className='-mt-2'>
-                <p className="font-semibold"> • CS CASTINGS PVT. LTD.</p>
-                <p className="font-semibold"> • OPPO POWER GRID, NEAR VILLAGE KHUMBH , AMLOH ROAD , MANDI GOBINDGARH </p>
+                <p className="font-bold text-xl -mt-3 mb-1">  C.S CASTINGS PVT. LTD.</p>
+                <p className='mb-2.5 text-[#1168b5] font-semibold' >[A Unit of Hardesh Group Of Companies]</p>
+                <p className="font-semibold">  VILL - KUMBH ,OPP POWER GRID,AMLOH ROAD , MANDI GOBINDGARH - 147301 (PB) </p>
               </div>
               <div  >
              <hr style={{ width: '106%',marginLeft:'-17px', marginTop: '-30px',marginBottom:'10px', height: '2px', backgroundColor: 'black', border: 'none' }} />
 
 
               <br />
-                <p >• PARTY NAME - {data.partyName}</p>
+                <p className='mb-1.5'>• PARTY NAME - {data.partyName}</p>
                 <p >• ADDRESS- {data.partyAddress}</p>
               </div>
             </div>
@@ -49,7 +60,7 @@ const CertificatePreview = ({ data }) => {
                 <div className="space-y-1">
                   <div className="text-l">T.C NO : {data.tcNumber || 'N/A'}</div>
                   <div className="text-l">Invoice No : {data.invoiceNo || 'N/A'}</div>
-                  <div className="text-l">Po : {data.purchaseOrder}</div>
+                  <div className="text-l">P.O : {data.purchaseOrder}</div>
                 </div>
                 <div className="space-y-1 text-right">
                   <div className="text-l">Date : {format(data.date, 'dd/MM/yy')}</div>
@@ -63,12 +74,12 @@ const CertificatePreview = ({ data }) => {
             <table className="w-full border border-black border-collapse text-xs mb-10">
               <thead>
                 <tr>
-                  <th className="border border-black py-2">S.no</th>
-                  <th className="border border-black py-2">Roll no</th>
+                  <th className="border border-black py-2">S.No</th>
+                  <th className="border border-black py-2">Roll No</th>
                   <th className="border border-black py-2">Roll Dimensions</th>
                   <th className="border border-black py-2">Grade</th>
                   <th className="border border-black py-2">Hardness</th>
-                  <th className="border border-black py-2">Grade colour</th>
+                  <th className="border border-black py-2">Colour Code</th>
                 </tr>
               </thead>
               <tbody>
@@ -102,7 +113,7 @@ const CertificatePreview = ({ data }) => {
               </colgroup>
               <thead>
                 <tr>
-                  {["ROLL No", "C", "SI", "MN", "S", "P", "CR", "NI", "MO", "V", "MG", "CU", "TI"].map((head, i) => (
+                  {["ROLL No", "C", "MN", "SI", "S", "P", "CR", "NI", "MO", "V", "MG", "CU", "TI"].map((head, i) => (
                     <th key={i} className="border border-black py-4 -pt-5">{head}</th>
                   ))}
                 </tr>
@@ -111,18 +122,18 @@ const CertificatePreview = ({ data }) => {
                 {data.items.map((item, index) => (
                   <tr key={index}>
                     <td className="border border-black h-10 text-center">{item.rollNo || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.C || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.SI || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.MN || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.S || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.P || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.CR || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.NI || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.MO || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.V || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.MG || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.CU || '-'}</td>
-                    <td className="border border-black h-10 text-center">{item.chemicalProperties.TI || '-'}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.C)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.MN)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.SI)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.S)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.P)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.CR)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.NI)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.MO)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.V)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.MG)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.CU)}</td>
+                    <td className="border border-black h-10 text-center">{formatDecimal(item.chemicalProperties.TI)}</td>
                   </tr>
                 ))}
                 {[...Array(Math.max(0, 6 - data.items.length))].map((_, index) => (
